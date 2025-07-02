25991b7982c82546bfa779dc361865e1605eeb1e
Alexandre Younes <younes.alexandre@outlook.com>
2025-07-02 05:27:56 +0300

    Update global_music.pme to correct music track references, remove obsolete HCA file, and adjust AssemblyInfo version for consistency.

--- obj/Debug/net6.0-windows/BGMSelector.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/BGMSelector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BGMSelector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd2193cc982e5c6ffb649a59f06f4027699b8fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee60ecd4e11793efdf3c0144a6fdeb4aad7730bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BGMSelector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BGMSelector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
