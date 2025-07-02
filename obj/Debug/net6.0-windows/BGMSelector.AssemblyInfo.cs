89762bfd30ddff4fc670be2e477a11c5da337e1c
Alexandre Younes <younes.alexandre@outlook.com>
2025-07-02 05:33:09 +0300

    ttt

--- obj/Debug/net6.0-windows/BGMSelector.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/BGMSelector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BGMSelector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d563374e3faf16abbe6f18c46f10dde45a84508c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35006a8576c472619e1fa63528c2f00c534ab42b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BGMSelector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BGMSelector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
